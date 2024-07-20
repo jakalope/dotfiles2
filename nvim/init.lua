40d0488cf92e166d877e41c896d817061b07f859
Jake Askeland <jake.askeland@gmail.com>
2024-07-20 10:51:42 -0700

    turn off fauxpilot

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,6 +74,17 @@ require('mason-lspconfig').setup({
   },
 })
 
+--require('copilot').setup({
+--  host = 'http://localhost:5000',
+--  model = 'py-model',
+--  max_tokens = 100,
+--  max_lines = 1000,
+--  max_num_results = 4,
+--  temperature = 0.6,
+--  keymap = {
+--    ['<C-Space>'] = '<Plug>(copilot-complete)',
+--  }
+--})
 
 require('setup-clipboard')
 require('buffer-delete')
